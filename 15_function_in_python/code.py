34809563ce85ffe4f69ea3103a1006ddb8aaf565
erk.mustech <erk.mustech@gmail.com>
2020-08-15 15:28:58 +0200

    change in local

--- 15_function_in_python/code.py
+++ 15_function_in_python/code.py
@@ -6,9 +6,9 @@ hello()
 
 print("---next 1---")
 
-def user_age_in_sec();
+def user_age_in_sec():
   user_age=int(input("Inter your age!"))
-  age-second= user_age*365* 24* 60* 60
+  age_second= user_age*365* 24* 60* 60
   print(f"your age is {age_second} second")
 
 user_age_in_sec()
