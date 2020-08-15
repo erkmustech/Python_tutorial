e642d5358ec5061ad6bd538fcd95f392481bbf83
erk.mustech <43365047+erkmustech@users.noreply.github.com>
2020-08-15 15:26:32 +0200

    update in hithub

--- 15_function_in_python/code.py
+++ 15_function_in_python/code.py
@@ -3,3 +3,16 @@ def hello():
 
 hello()
 
+
+print("---next 1---")
+
+def user_age_in_sec();
+  user_age=int(input("Inter your age!"))
+  age-second= user_age*365* 24* 60* 60
+  print(f"your age is {age_second} second")
+
+user_age_in_sec()
+
+    
+
+
